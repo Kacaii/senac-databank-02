d53e817858d2e997e7393545785f509f13301192
Kacaii <kacaii.privado@gmail.com>
2025-05-09 20:06:14 -0300

    add relatorio_4 again

---
+++ BDII_2_exercicio_de_BDII_DQL_JOIN/Relatorio_4.sql
@@ -0,0 +1,23 @@
+use petshop;
+
+select
+    e.nome,
+    e.cpf,
+    e.sexo,
+    concat('R$ ', round(e.salario, 2)) as salario,
+    count(v.empregado_cpf) as vendas_realizadas,
+    concat(
+        'R$ ',
+        coalesce(
+            sum((v.valor - v.desconto)),
+            0
+        )
+    ) as valor_total_vendas,
+    concat('R$ ', sum(e.comissao)) as total_comissao
+from
+    empregado as e
+left join
+    venda as v
+    on e.cpf = v.empregado_cpf
+group by e.cpf
+order by vendas_realizadas desc;
