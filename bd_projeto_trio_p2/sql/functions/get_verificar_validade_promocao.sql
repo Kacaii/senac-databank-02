0bc4b0465b7af014b36ca1111d3d7ea9885b7bfc
Kacaii <kacaii.privado@gmail.com>
2025-06-11 15:38:38 -0300

    add get_verificar_validade_promocao.sql

---
+++ bd_projeto_trio_p2/sql/functions/get_verificar_validade_promocao.sql
@@ -0,0 +1,26 @@
+USE loja_de_roupas;
+
+START TRANSACTION;
+
+DELIMITER $$
+CREATE FUNCTION verificar_validade_promocao(p_id_promocao INT) RETURNS BOOLEAN
+BEGIN
+DECLARE
+v_valida BOOLEAN;
+
+SELECT
+  ativo = TRUE
+  AND CURDATE() BETWEEN data_inicio AND IFNULL(data_fim, CURDATE()) INTO v_valida
+FROM
+  Promocao
+WHERE
+  id = p_id_promocao;
+
+RETURN IFNULL(v_valida, FALSE);
+
+END $$
+DELIMITER ;
+
+COMMIT;
+
+-- drop function verificar_validade_promocao;
