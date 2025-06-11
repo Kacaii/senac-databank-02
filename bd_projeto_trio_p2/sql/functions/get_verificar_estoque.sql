b5f16f59a6c44f214a22bb314d755d7e232f9053
Kacaii <kacaii.privado@gmail.com>
2025-06-11 15:24:36 -0300

    add function verificar_estoque.sql

---
+++ bd_projeto_trio_p2/sql/functions/get_verificar_estoque.sql
@@ -0,0 +1,23 @@
+START TRANSACTION;
+
+DELIMITER $$
+CREATE FUNCTION verificar_estoque(p_id_produto INT) RETURNS INT READS SQL DATA
+BEGIN
+DECLARE
+v_quantidade INT;
+
+SELECT
+  SUM(quantidade) INTO v_quantidade
+FROM
+  Estoque
+WHERE
+  id_produto = p_id_produto;
+
+RETURN IFNULL(v_quantidade, 0);
+
+END $$
+DELIMITER ;
+
+COMMIT;
+
+-- drop function verificar_estoque;
