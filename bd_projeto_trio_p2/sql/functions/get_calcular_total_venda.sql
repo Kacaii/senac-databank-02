ad77fb4ad72357c426a9e2536c1b969592b5b8fa
Kacaii <kacaii.privado@gmail.com>
2025-06-11 15:30:36 -0300

    add get_calcular_total_venda.sql

---
+++ bd_projeto_trio_p2/sql/functions/get_calcular_total_venda.sql
@@ -0,0 +1,23 @@
+USE loja_de_roupas;
+
+START TRANSACTION;
+
+DELIMITER $$
+CREATE FUNCTION calcular_total_venda(p_id_venda INT) RETURNS DECIMAL(10, 2) DETERMINISTIC
+BEGIN
+DECLARE
+v_total DECIMAL(10, 2);
+
+SELECT
+  SUM(preco_unitario * quantidade) INTO v_total
+FROM
+  Item_Venda
+WHERE
+  id_venda = p_id_venda;
+
+RETURN COALESCE(v_total, 0);
+
+END $$
+DELIMITER ;
+
+COMMIT;
