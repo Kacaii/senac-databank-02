0b303a7e21ae9ca82d04b2654a8a01c462293245
Kacaii <kacaii.privado@gmail.com>
2025-06-11 18:08:20 -0300

    add sp_relatorio_vendas_periodo

--- bd_projeto_trio_p2/sql/procedures/all_procedures.sql
+++ bd_projeto_trio_p2/sql/procedures/all_procedures.sql
@@ -2,14 +2,16 @@ USE loja_de_roupas;
 
 START TRANSACTION;
 
-call sp_create_views();
-
-call sp_create_functions();
+call sp_all_alter_tables();
 
 call sp_all_inserts();
 
 call sp_all_updates();
 
-call sp_all_alter_tables();
+call sp_create_functions();
+
+call sp_create_views();
+
+call sp_relatorio_vendas_periodo();
 
 COMMIT;
---
+++ bd_projeto_trio_p2/sql/procedures/sp_relatorio_vendas_periodo.sql
@@ -0,0 +1,36 @@
+USE loja_de_roupas;
+
+START TRANSACTION;
+
+DELIMITER $$
+CREATE PROCEDURE sp_relatorio_vendas_periodo(IN p_data_inicio DATE, IN p_data_fim DATE)
+BEGIN
+
+SELECT
+  v.id AS id_venda,
+  v.data_venda,
+  c.nome AS cliente,
+  f.nome AS funcionario,
+  fp.nome AS forma_pagamento,
+  SUM(iv.preco_unitario * iv.quantidade) AS total_venda
+FROM
+  Venda v
+  JOIN Cliente c ON v.id_cliente = c.id
+  JOIN Funcionario f ON v.id_funcionario = f.id
+  JOIN Forma_Pagamento fp ON v.id_forma_pagamento = fp.id
+  JOIN Item_Venda iv ON v.id = iv.id_venda
+WHERE
+  v.data_venda BETWEEN p_data_inicio AND p_data_fim
+GROUP BY
+  v.id,
+  v.data_venda,
+  c.nome,
+  f.nome,
+  fp.nome
+ORDER BY
+  v.data_venda;
+
+END $$
+DELIMITER ;
+
+COMMIT;
